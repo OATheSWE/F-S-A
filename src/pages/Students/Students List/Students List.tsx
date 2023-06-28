5d78618450dec9751a2d4ce36cd72e99ca7e1fe3
OATheSWE <osborneosas12@gmail.com>
2023-06-28 16:33:14 +0100

    update

--- src/pages/Students/Students List/Students List.tsx
+++ src/pages/Students/Students List/Students List.tsx
@@ -1,49 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../../../firebase-config';
+import { collection, query, orderBy, limit, getDocs, DocumentData } from 'firebase/firestore';
 import { FaMapMarkerAlt, MdDelete, MdModeEditOutline } from '../../../assets/IconImports';
 
+type Student = {
+  name: string;
+  bookOfStudy: string;
+};
+
 const StudentsList: React.FC = () => {
+  const [students, setStudents] = useState<Student[]>([]);
 
-    return (
+  useEffect(() => {
+    const fetchStudents = async () => {
+      const studentsCollectionRef = collection(db, 'Students');
+      const studentsQuery = query(studentsCollectionRef, orderBy('time', 'desc'));
+      const querySnapshot = await getDocs(studentsQuery);
 
-        <div className="list-container">
-            <div className="popup text-white rounded students-list">
-                <form>
-                    <h3>John Doe</h3>
-                    <p>Enjoy life forever lesson 1, point 3</p>
-                    <div>
-                        Trace Location
-                        <FaMapMarkerAlt className="icon-locate" />
-                    </div>
-                    <MdDelete className="icon-delete" />
-                    <MdModeEditOutline className="icon-edit" />
-                </form>
+      const studentData = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          name: data.name,
+          bookOfStudy: data.bookOfStudy,
+        };
+      });
+
+      setStudents(studentData);
+    };
+
+    fetchStudents();
+  }, []);
+
+  return (
+    <div className="list-container">
+      {students.map((student, index) => (
+        <div className="popup text-white rounded students-list" key={index}>
+          <form>
+            <h3>{student.name}</h3>
+            <p>{student.bookOfStudy}</p>
+            <div>
+              Trace Location
+              <FaMapMarkerAlt className="icon-locate" />
             </div>
-            <div className="popup text-white rounded students-list">
-                <form>
-                    <h3>John Doe</h3>
-                    <p>Enjoy life forever lesson 1, point 3</p>
-                    <div>
-                        Trace Location
-                        <FaMapMarkerAlt className="icon-locate" />
-                    </div>
-                    <MdDelete className="icon-delete" />
-                    <MdModeEditOutline className="icon-edit" />
-                </form>
-            </div>
-            <div className="popup text-white rounded students-list">
-                <form>
-                    <h3>John Doe</h3>
-                    <p>Enjoy life forever lesson 1, point 3</p>
-                    <div>
-                        Trace Location
-                        <FaMapMarkerAlt className="icon-locate" />
-                    </div>
-                    <MdDelete className="icon-delete" />
-                    <MdModeEditOutline className="icon-edit" />
-                </form>
-            </div>
+            <MdDelete className="icon-delete" />
+            <MdModeEditOutline className="icon-edit" />
+          </form>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default StudentsList;
